c8653647a75252b5e2bfec918d47321f5960a4e3
yc-song <57248081+yc-song@users.noreply.github.com>
2022-04-30 00:31:26 +0900

    Create main.cpp

---
+++ Heap/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "heapify.cpp"
+using namespace std;
+int main() {
+	heap h;
+	h.insert(16);
+	h.insert(4);
+	h.insertarray(a);
+	h.showcontents();
+	//h.showcontents();
+	return 0;
+}
