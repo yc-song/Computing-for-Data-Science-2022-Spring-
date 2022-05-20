8473b23ed766e36f54879f606401306ce03bcf81
yc-song <57248081+yc-song@users.noreply.github.com>
2022-05-20 23:59:21 +0900

    Update main.cpp

--- Homework/Assignment 6/main.cpp
+++ Homework/Assignment 6/main.cpp
@@ -1,168 +1,42 @@
+#include <cstdlib>
 #include <iostream>
-#include "bst.h"
+#include "myset.h"
+#include <stdlib.h>
+#include <time.h> // 헤더 파일
 
-using namespace std;
+int main(int argc, char** argv) {
+  MySet myset;
 
-void BST::Insert(int key) {
-  Node* z = new Node(key);
+  myset.Insert(3);
+  myset.Insert(3);
+  myset.Insert(3);
+  myset.Insert(7);
+  myset.Insert(10);
+  myset.Insert(23);
+  myset.Insert(22);
 
-  Node* x = this->root;
-  Node* y = NULL;
-  while (x != NULL) {
-    y = x;
-    if (z->key < x->key) {
-        x = x->left;
-    }
-    else if (z->key > x->key) {
-        x = x->right;
-    }
-    else if (z->key == x->key) return;
-  }
-  size++;
-  // found the location - insert z with parent y
-  z->parent = y;
+  myset.Insert(8);
+  std::cout << "Size of the set is " << myset.Size() << std::endl;
 
-  if (y == NULL) {
-    root = z;
-  }
-  else if (z->key < y->key) {
-    y->left = z;
-  }
-  else {
-    y->right = z;
-  }
+  myset.Erase(14);
+  myset.Erase(3);
+  myset.Erase(22);
+
+  myset.Insert(23);
+  myset.Insert(100);
+
+  std::cout << "Size of the set after inserts and erases is "
+    << myset.Size() << std::endl;
+
+  std::cout << "Is number '32' exists in the set ? "
+    << myset.Find(32) << std::endl;
+
+  std::cout << "Is number '22' exists in the set ? "
+    << myset.Find(22) << std::endl;
+
+  std::cout << "Is number '100' exists in the set ? "
+    << myset.Find(100) << std::endl;
+  return EXIT_SUCCESS;
 }
 
-void BST::transplant(Node* u, Node* v){
-	if (u->parent == NULL) {
-		root = v;
-	} else if (u == u->parent->left) {
-		u->parent->left = v;
-	} else{
-		u->parent->right = v;
-	}
 
-	if (v != NULL) {
-		v->parent = u->parent;
-	}
-}
-
-void BST::Delete(Node* z) {
-  Node* y;
-  
-	if (z->left == nullptr) {
-		transplant(z, z->right);
-	}
-  else if (z->right == nullptr) {
-		transplant(z, z->left);
-	}
-  else {
-		y = Minimum(z->right);
-		if (y->parent != z) {
-			transplant(y, y->right);
-			y->right = z->right;
-			y->right->parent = y;
-		}
-		transplant(z, y);
-		y->left = z->left;
-		y->left->parent = y;
-	}
-}
-
-void BST::Preorder(Node* node) {
-  if (node != NULL) {
-    cout << node->key << " ";
-    Preorder(node->left);
-    Preorder(node->right);
-  } 
-}
-
-void BST::Inorder(Node* node) {
-  if (node != NULL) {
-    Inorder(node->left);
-    cout << node->key << " ";
-    Inorder(node->right);
-  } 
-}
-
-void BST::Postorder(Node* node) {
-  if (node != NULL) {
-    Postorder(node->left);
-    Postorder(node->right);
-    cout << node->key << " ";
-  } 
-}
-
-BST::Node* BST::Search(Node* node, int key) {
-  if (node == NULL || key == node->key) {
-    return node;
-  }
-
-  if (key < node->key)
-    return Search(node->left, key);
-  else
-    return Search(node->right, key);
-}
-
-BST::Node* BST::Maximum(Node* node) {
-  while (node->right != NULL) {
-    node = node->right;
-  }
-  return node;
-}
-
-BST::Node* BST::Minimum(Node* node) {
-  while (node->left != NULL) {
-    node = node->left;
-  }
-  return node;
-}
-
-BST::Node* BST::Successor(Node* x) {
-  if (x->right != NULL) {
-    return Minimum(x->right);
-  }
-
-  Node* y = x->parent;
-  while (y != NULL && x == y->right) {
-    x = y;
-    y = y->parent;
-  }
-  return y;
-}
-
-BST::Node* BST::Predecessor(Node* x) {
-  if (x->left != NULL) {
-    return Maximum(x->left);
-  }
-
-  Node* y = x->parent;
-  while (y != NULL && x == y->left) {
-    x = y;
-    y = y->parent;
-  }
-  return y;
-}
-
-void BST::_print(Node* root, string indent, bool last) {
-  if (root != NULL) {
-    cout << indent;
-    if (last) {
-      cout << "└────";
-      indent += "     ";
-    } else {
-      cout << "├────";
-      indent += "|    ";
-    }
-
-    cout << root->key << endl;
-
-    _print(root->left, indent, false);
-    _print(root->right, indent, true);
-  }
-}
-
-void BST::Print() {
-  if (GetRoot() != NULL)
-    _print(GetRoot(), "", true);
-}
